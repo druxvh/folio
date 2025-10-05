c9254fe0ddd3aa0f6d0b2527378c36700fb3267d
dm <druavhm@gmail.com>
2025-10-06 04:08:41 +0530

    chore: configure next.js for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
     qualities: [25, 50, 75, 100],
+    unoptimized: true,
   }
 };
 
